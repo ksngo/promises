ae1db3e4a58d7ca5f1ae661e96caf6c4e2197a14
ksngo <sevenelectrons@gmail.com>
2021-05-01 11:47:24 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # promises
+
+node index.js
+localhost:3000/static/chapter-1/1-01-async-xhr.html
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use('/static',express.static(path.join(__dirname,'promises-book-examples')))
+
+
+app.get('/', (req, res)=>{
+    res.send("hello")
+})
+
+app.listen(port, ()=>{
+    console.log("App listening at port 3000.")
+})
